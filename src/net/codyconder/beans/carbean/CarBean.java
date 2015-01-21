67bb4d5f4d6238fd47acb3023c441b2396471834
Cody Conder <cconder89@gmail.com>
2015-01-21 12:28:37 -0700

    CarBean.java: Added an additional constructor; added hashCode and equals methods. Car.java: Updated to take advantage of the new constructor; removed file output. GarageBean.java: Started developing a Garage Bean to store the Car Beans. This is very much in the early stages.

--- src/net/codyconder/beans/carbean/CarBean.java
+++ src/net/codyconder/beans/carbean/CarBean.java
@@ -11,11 +11,20 @@ public class CarBean implements java.io.Serializable {
 	
 	// No Arg Constructor
 	public CarBean () {
-		year = 0;
-		make = "";
-		model = "";
-		color = "";
-		owner = "";
+		this.year = 0;
+		this.make = "";
+		this.model = "";
+		this.color = "";
+		this.owner = "";
+	}
+	
+	// Full Constructor
+	public CarBean (int year, String make, String model, String color, String owner) {
+		this.year = year;
+		this.make = make;
+		this.model = model;
+		this.color = color;
+		this.owner = owner;
 	}
 	
 	public void setYear (final int aYear) {
@@ -56,6 +65,52 @@ public class CarBean implements java.io.Serializable {
 	
 	public String getOwner() {
 		return this.owner;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + ((make == null) ? 0 : make.hashCode());
+		result = prime * result + ((model == null) ? 0 : model.hashCode());
+		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+		result = prime * result + year;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CarBean other = (CarBean) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (make == null) {
+			if (other.make != null)
+				return false;
+		} else if (!make.equals(other.make))
+			return false;
+		if (model == null) {
+			if (other.model != null)
+				return false;
+		} else if (!model.equals(other.model))
+			return false;
+		if (owner == null) {
+			if (other.owner != null)
+				return false;
+		} else if (!owner.equals(other.owner))
+			return false;
+		if (year != other.year)
+			return false;
+		return true;
 	}
 	
 }
