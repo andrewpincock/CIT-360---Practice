41f577d5d2088b63f2e9fbc17063c98b7a620465
Cody Conder <cconder89@gmail.com>
2015-01-29 12:17:03 -0700

    Renamed SocketServerTest to SocketServer. Modified the client test Added the server null test

--- src/net/codyconder/clientserver/ServerNullTest.java
+++ src/net/codyconder/clientserver/ServerNullTest.java
@@ -4,30 +4,35 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import net.codyconder.beans.carbean.OwnerBean;
+//import net.codyconder.beans.carbean.OwnerBean;
 
 import org.quickconnectfamily.json.JSONException;
 import org.quickconnectfamily.json.JSONInputStream;
 import org.quickconnectfamily.json.JSONOutputStream;
 
-public class SocketClientTest {
+/*
+ * This tests to see what happens when we send a null to the server.
+ * The server will hang until this thread is terminated.
+ * The server will then crash (bang, boom).
+ */
+
+public class ServerNullTest {
 
 	public static void main(String[] args) throws UnknownHostException, IOException, JSONException {
 		// Create an owner
-		OwnerBean anOwner = new OwnerBean(1,"Cody","Conder","male");
-		
+		// OwnerBean anOwner = new OwnerBean(1,"Cody","Conder","male");
+
 		Socket toServer = new Socket("127.0.0.1", 9889);
-		
+		System.out.println("Connection made");
 		JSONInputStream inFromServer = new JSONInputStream(toServer.getInputStream());
 		JSONOutputStream outToServer = new JSONOutputStream(toServer.getOutputStream());
-		
-		outToServer.writeObject(anOwner);
-		
+		System.out.println("Streams set up");
+		outToServer.writeObject(null);
+		System.out.println("Object sent (ha ha)");
 		System.out.println(inFromServer.readObject());
 		
 		toServer.close();
 		System.out.println("Connection closed");
-
 	}
 
 }
---
+++ src/net/codyconder/clientserver/SocketServer.java
@@ -0,0 +1,48 @@
+package net.codyconder.clientserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+import org.quickconnectfamily.json.JSONException;
+import org.quickconnectfamily.json.JSONInputStream;
+import org.quickconnectfamily.json.JSONOutputStream;
+
+import net.codyconder.beans.carbean.OwnerBean;
+
+public class SocketServer {
+
+	public static void main(String[] args) throws IOException, JSONException {
+		while (true) {
+			ServerSocket listeningSocket = new ServerSocket(9889);
+			System.out.println("Listening...");
+			Socket clientSocket = listeningSocket.accept();
+			System.out.println("Connection made");
+
+			JSONInputStream inFromClient = new JSONInputStream(
+					clientSocket.getInputStream());
+			JSONOutputStream outToClient = new JSONOutputStream(
+					clientSocket.getOutputStream());
+			
+			System.out.println("Streams set up");
+			@SuppressWarnings("unchecked")
+			HashMap<String, ?> parsedJSONMap = (HashMap<String, ?>) inFromClient
+					.readObject();
+			
+			System.out.println("Object read");
+			OwnerBean theOwner = new OwnerBean(parsedJSONMap);
+
+			String message = "The owner has been created successfully.";
+
+			outToClient.writeObject(message);
+
+			System.out.println("The owner is " + theOwner.getFirstName() + " "
+					+ theOwner.getLastName() + ".");
+
+			listeningSocket.close();
+		}
+
+	}
+
+}
