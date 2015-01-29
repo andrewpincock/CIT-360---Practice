470f694bced54153d938ae46c7ea6962db5c014d
Cody Conder <cconder89@gmail.com>
2015-01-28 21:23:29 -0700

    Two-way communication between the Server and the Client.

--- src/net/codyconder/clientserver/SocketClientTest.java
+++ src/net/codyconder/clientserver/SocketClientTest.java
@@ -4,16 +4,28 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import net.codyconder.beans.carbean.OwnerBean;
+
+import org.quickconnectfamily.json.JSONException;
 import org.quickconnectfamily.json.JSONInputStream;
 import org.quickconnectfamily.json.JSONOutputStream;
 
 public class SocketClientTest {
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
+	public static void main(String[] args) throws UnknownHostException, IOException, JSONException {
+		// Create an owner
+		OwnerBean anOwner = new OwnerBean(1,"Cody","Conder","male");
+		
 		Socket toServer = new Socket("127.0.0.1", 9889);
 		
 		JSONInputStream inFromServer = new JSONInputStream(toServer.getInputStream());
 		JSONOutputStream outToServer = new JSONOutputStream(toServer.getOutputStream());
+		
+		outToServer.writeObject(anOwner);
+		
+		System.out.println(inFromServer.readObject());
+		
+		toServer.close();
 
 	}
 
--- src/net/codyconder/clientserver/SocketServerTest.java
+++ src/net/codyconder/clientserver/SocketServerTest.java
@@ -3,18 +3,36 @@ package net.codyconder.clientserver;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
+import org.quickconnectfamily.json.JSONException;
 import org.quickconnectfamily.json.JSONInputStream;
 import org.quickconnectfamily.json.JSONOutputStream;
 
+import net.codyconder.beans.carbean.OwnerBean;
+
 public class SocketServerTest {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, JSONException {
 		ServerSocket listeningSocket = new ServerSocket(9889);
+		System.out.println("Listening...");
 		Socket clientSocket = listeningSocket.accept();
 		
 		JSONInputStream inFromClient = new JSONInputStream(clientSocket.getInputStream());
 		JSONOutputStream outToClient = new JSONOutputStream(clientSocket.getOutputStream());
+		
+		@SuppressWarnings("unchecked")
+		HashMap<String, ?> parsedJSONMap = (HashMap<String, ?>) inFromClient.readObject();
+		
+		OwnerBean theOwner = new OwnerBean(parsedJSONMap);
+		
+		String message = "The owner has been created successfully.";
+		
+		outToClient.writeObject(message);
+		
+		System.out.println("The owner is " + theOwner.getFirstName() + " " + theOwner.getLastName() + ".");
+		
+		listeningSocket.close();
 
 	}
 
