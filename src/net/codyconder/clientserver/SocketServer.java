6e08b9ca1482efeea8878140d428d197c55857b1
Cody Conder <cconder89@gmail.com>
2015-02-04 20:42:33 -0700

    Added comments to the SocketServer.java file

--- src/net/codyconder/clientserver/SocketServer.java
+++ src/net/codyconder/clientserver/SocketServer.java
@@ -11,42 +11,61 @@ import org.quickconnectfamily.json.JSONOutputStream;
 
 import net.codyconder.beans.carbean.OwnerBean;
 
+/* 
+ * This is a simple server.
+ * It expects a JSON string and converts it to an OwnerBean.
+ * The server is configured to run in an infinite loop.
+ * The server is also configured to handle all the errors it has experienced through my testing so far without crashing.
+ * The various client files demonstrate different things that the client could send. Most of them are not happy ;)
+ */
+
 public class SocketServer {
 
 	public static void main(String[] args) throws IOException, JSONException {
 		while (true) {
+			// -------------------------------------------------------------------------------------- //
+			// The various println statements indicate where we are in the execution of the program.  //
+			// -------------------------------------------------------------------------------------- //
+			
+			// Create a server socket to listen on port 9889
 			ServerSocket listeningSocket = new ServerSocket(9889);
 			System.out.println("Listening...");
 			Socket clientSocket = listeningSocket.accept();
 			System.out.println("Connection made");
 
-			JSONInputStream inFromClient = new JSONInputStream(
-					clientSocket.getInputStream());
-			JSONOutputStream outToClient = new JSONOutputStream(
-					clientSocket.getOutputStream());
-			
+			// Set up our JSON input and output streams
+			JSONInputStream inFromClient = new JSONInputStream(clientSocket.getInputStream());
+			JSONOutputStream outToClient = new JSONOutputStream(clientSocket.getOutputStream());
 			System.out.println("Streams set up");
+			
+			// The operation used to read the OwnerBean object is surrounded in a try/catch block.
+			// We will catch various exceptions that we might encounter in order to keep the server up and running.
 			try {
 				@SuppressWarnings("unchecked")
-				HashMap<String, ?> parsedJSONMap = (HashMap<String, ?>) inFromClient
-						.readObject();
-				
+				// Read the OwnerBean into a HashMap
+				HashMap<String, ?> parsedJSONMap = (HashMap<String, ?>) inFromClient.readObject();
 				System.out.println("Object read");
+				
+				// Create a new OwnerBean
 				OwnerBean theOwner = new OwnerBean(parsedJSONMap);
 
+				// Send a success message back to the client
 				String message = "The owner has been created successfully.";
-
 				outToClient.writeObject(message);
-
-				System.out.println("The owner is " + theOwner.getFirstName() + " "
-						+ theOwner.getLastName() + ".");
+				
+				// Print out the owner details on our side
+				System.out.println("The owner is " + theOwner.getFirstName() + " " 
+						+ theOwner.getLastName() + ", a " + theOwner.getGender() + ".");
 			} catch (JSONException e) {
-				// TODO Auto-generated catch block
+				// This catches the exception thrown if we receive an invalid JSON string or a null object.
 				System.out.println("An invalid JSON string was received. Resetting connection.");
 			} catch (ClassCastException e) {
+				// This takes care of the issue if we receive something that's not an OwnerBean.
 				System.out.println("An invalid object was received. Resetting connection.");
 			}
-
+			
+			// Close the connection with the client.
+			// The loop will repeat and we will open up another listening socket.
 			listeningSocket.close();
 		}
 
