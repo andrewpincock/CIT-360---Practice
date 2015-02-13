72ff629065a80b358409faac5d5a3c255d795831
Cody Conder <cconder89@gmail.com>
2015-02-13 12:31:33 -0700

    Completed the Car entity

--- src/net/codyconder/hibernate/practice/Car.java
+++ src/net/codyconder/hibernate/practice/Car.java
@@ -12,21 +12,54 @@ public class Car {
 	@Id
 	@GeneratedValue
 	private Integer id;
-	private Integer year;
+	private String year;
 	private String make;
 	private String model;
 	private String color;
 	
-	public Integer getYear() {
+	public Car () {
+		
+	}
+	
+	public String toString() {
+		return "Car: [id=" + id + ", year=" + year + ", make=" + make 
+				+ ", model=" + model + ", color=" + color + "]";
+	}
+	
+	public Integer getId() {
+		return this.id;
+	}
+	
+	public void setId(Integer anId) {
+		this.id = anId;
+	}
+	
+	public void setYear(String aYear) {
+		this.year = aYear;
+	}
+	
+	public String getYear() {
 		return this.year;
+	}
+	
+	public void setMake(String aMake) {
+		this.make = aMake;
 	}
 	
 	public String getMake() {
 		return this.make;
 	}
 	
+	public void setModel(String aModel) {
+		this.model = aModel;
+	}
+	
 	public String getModel() {
 		return this.model;
+	}
+	
+	public void setColor(String aColor) {
+		this.color = aColor;
 	}
 	
 	public String getColor() {
