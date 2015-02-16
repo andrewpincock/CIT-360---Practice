62fad1998281134daf6fa98533373c2b6d3edbfc
Cody Conder <cconder89@gmail.com>
2015-02-15 22:27:23 -0700

    Added PracticeSample.java as stub. Will create a test case.

---
+++ src/net/codyconder/hibernate/practice/PracticeSample.java
@@ -0,0 +1,14 @@
+package net.codyconder.hibernate.practice;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.Query;
+
+public class PracticeSample {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
