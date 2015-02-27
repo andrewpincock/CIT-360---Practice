8b657cb17a21b82bf037466bdd8d2816318e95a7
Cody Conder <cconder89@gmail.com>
2015-02-26 22:30:34 -0700

    Fixed code formatting

--- src/net/codyconder/hibernate/working/Program.java
+++ src/net/codyconder/hibernate/working/Program.java
@@ -5,15 +5,13 @@ import java.util.Scanner;
 public class Program {
 
 	/*
-	 * Create a program object. This makes it available
-	 * to all methods within the class without having
-	 * to instantiate it each time you need it.
+	 * Create a program object. This makes it available to all methods within
+	 * the class without having to instantiate it each time you need it.
 	 */
 	static Program program = new Program();
 	OwnerManagement ownerManagement = new OwnerManagement();
 	CarManagement carManagement = new CarManagement();
 	Scanner userInput = new Scanner(System.in);
-	
 
 	// Program constructor
 	public Program() {
@@ -22,9 +20,9 @@ public class Program {
 	public static void main(String[] args) {
 		boolean quitProgram = false;
 		while (!quitProgram) {
-			/* 
-			 * Control of the program will flow through the
-			 * Main Menu and the sub menus.
+			/*
+			 * Control of the program will flow through the Main Menu and the
+			 * sub menus.
 			 */
 			quitProgram = program.mainMenu();
 		}
@@ -32,18 +30,18 @@ public class Program {
 	}
 
 	/* ************
-	 * System Menus
+	 * System Menus 
 	 * ************/
-	
+
 	/*
 	 * Main Menu
 	 */
 	private boolean mainMenu() {
 		boolean quitProgram = false;
-		
+
 		while (!quitProgram) {
 			Integer selection = null;
-			
+
 			// Print the main menu
 			System.out.println();
 			System.out.println("Main Menu");
@@ -51,7 +49,7 @@ public class Program {
 			System.out.println("	2. Car Management");
 			System.out.println("	3. Quit application");
 			System.out.print("Please enter your selection (1-3): ");
-			
+
 			// Attempt to read user input
 			try {
 				selection = Integer.parseInt(userInput.nextLine());
@@ -62,7 +60,7 @@ public class Program {
 				System.out.println("Oops! Please enter a number (1-3) to use the system.");
 				return false;
 			}
-			
+
 			switch (selection) {
 			case 1:
 				// Open the Owner Menu
@@ -86,7 +84,7 @@ public class Program {
 				break;
 			}
 		}
-		
+
 		return quitProgram;
 
 	}
@@ -99,7 +97,7 @@ public class Program {
 
 		while (!returnToMainMenu) {
 			Integer selection = null;
-			
+
 			// Print the owner management menu
 			System.out.println();
 			System.out.println("Owner Management");
@@ -110,14 +108,14 @@ public class Program {
 			System.out.println("	5. Remove Owner");
 			System.out.println("	6. Return to Main Menu");
 			System.out.print("Please enter your selection (1-6): ");
-			
+
 			// Attempt to read user input
 			try {
 				selection = Integer.parseInt(userInput.nextLine());
 			} catch (NumberFormatException e) {
 				// Don't do anything. It will fall to the default case.
 			}
-			
+
 			switch (selection) {
 			case 1:
 				// Call the addOwner method, then return to the owner menu
@@ -128,7 +126,8 @@ public class Program {
 				ownerManagement.listOwners();
 				break;
 			case 3:
-				// Call the updateOwnerInformation method, then return to the owner menu
+				// Call the updateOwnerInformation method, then return to the
+				// owner menu
 				ownerManagement.updateOwnerInformation();
 				break;
 			case 4:
@@ -159,10 +158,10 @@ public class Program {
 	 */
 	private void carMenu() {
 		boolean returnToMainMenu = false;
-		
+
 		while (!returnToMainMenu) {
 			Integer selection = null;
-			
+
 			// Print the owner management menu
 			System.out.println();
 			System.out.println("Car Management");
@@ -173,33 +172,34 @@ public class Program {
 			System.out.println("	5. Remove Car");
 			System.out.println("	6. Return to Main Menu");
 			System.out.print("Please enter your selection (1-6): ");
-			
+
 			// Attempt to read user input
 			try {
 				selection = Integer.parseInt(userInput.nextLine());
 			} catch (NumberFormatException e) {
 				// Don't do anything. It will fall to the default case.
 			}
-			
+
 			switch (selection) {
 			case 1:
-				//Call the addCar method then return to the car menu
+				// Call the addCar method then return to the car menu
 				carManagement.addCar();
 				break;
 			case 2:
-				//Call the listAllCars method then return to the car menu
+				// Call the listAllCars method then return to the car menu
 				carManagement.listAllCars();
 				break;
 			case 3:
-				//Call the updateCarInformation method then return to the car menu
+				// Call the updateCarInformation method then return to the car
+				// menu
 				carManagement.updateCarInformation();
 				break;
 			case 4:
-				//Call the listOwnerCars method then return to the car menu
+				// Call the listOwnerCars method then return to the car menu
 				ownerManagement.listOwnerCars();
 				break;
 			case 5:
-				//Call the removeCar method then return to the car menu
+				// Call the removeCar method then return to the car menu
 				carManagement.removeCar();
 				break;
 			case 6:
