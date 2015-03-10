c998aa9f889ca1cbdb865b42bf7866686ba8fffe
Cody Conder <cconder89@gmail.com>
2015-03-09 22:31:02 -0600

    Thread example

---
+++ src/net/codyconder/threads/ThreadSocketServerMain.java
@@ -0,0 +1,36 @@
+package net.codyconder.threads;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ThreadSocketServerMain {
+
+	private static int port = 9889;
+
+	public static void main(String[] args) {
+		ServerSocket listeningSocket;
+		try {
+			listeningSocket = new ServerSocket(port);
+			Socket clientSocket;
+
+			while (true) {
+				try {
+					clientSocket = listeningSocket.accept();
+					ThreadSocketServer conn = new ThreadSocketServer(clientSocket);
+
+					Thread t = new Thread(conn);
+					t.start();
+				} catch (IOException e) {
+					System.out.println("IOException on socket listen: " + e);
+					e.printStackTrace();
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("IOException on socket listen: " + e);
+			e.printStackTrace();
+		}
+
+	}
+
+}
